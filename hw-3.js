f0f8cbf5c2d21cd79fdfba0bdbdf1110364ecb7f
Lada Antsyferova <mygi746@gmail.com>
2024-01-19 02:01:09 +0200

    hw-3

--- hw-3.js
+++ hw-3.js
@@ -1,5 +1,5 @@
 let first = +(prompt('Веди перше число'));
-let second= +(prompt('Веди перше число'));
-let third = +(prompt('Веди перше число'));
+let second= +(prompt('Веди друге число'));
+let third = +(prompt('Веди третє число'));
 
 alert((first + second + third)/3)
