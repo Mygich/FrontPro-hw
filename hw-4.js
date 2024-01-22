833c487edc1726b798f432e2f16cce4d4f6baf6a
Lada Antsyferova <mygi746@gmail.com>
2024-01-22 22:49:15 +0200

    cl-4

--- hw-4.js
+++ hw-4.js
@@ -1,15 +1,15 @@
-let number1 = +prompt('first number');
+/*let number1 = +prompt('first number');
 let number2 = +prompt('second number');
-let sign = prompt('Введіть операцію');
+let sign = prompt('Введіть операцію');*/
 
-if (sign === '+'){
-    console.log(number1 + number2);
-} else if (sign === '-'){
-    console.log(number1 - number2);
-} else if (sign === '*'){
-    console.log(number1 * number2);
-} else if (sign === '/'){
-    console.log(number1 / number2);
-} else {
-    console.log('Ви ввели не знак');
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+
+let number = 1
+
+while (number <=5){
+    console.log(number);
 }
