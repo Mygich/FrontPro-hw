fb38161643d159424eb70c0bc576c56b60b44202
Lada Antsyferova <mygi746@gmail.com>
2024-01-23 03:20:41 +0200

    hw-4

--- hw-4.js
+++ hw-4.js
@@ -1,15 +1,22 @@
-/*let number1 = +prompt('first number');
-let number2 = +prompt('second number');
-let sign = prompt('Введіть операцію');*/
 
-console.log(1);
-console.log(2);
-console.log(3);
-console.log(4);
-console.log(5);
+let number1 = +prompt('Введіть перше число:');
+let number2 = +prompt('Введіть друге число:');
 
-let number = 1
+let sum = number1 + number2;
+let difference = number1 - number2;
+let product = number1 * number2;
+let quotient = number1 / number2;
 
-while (number <=5){
-    console.log(number);
-}
+
+let newResult = alert(`Користувач ввів ${number1} і ${number2}:\n
+${number1} + ${number2} = ${sum} 
+${number1} - ${number2} = ${difference} 
+${number1} * ${number2} = ${product}
+${number1} / ${number2} = ${quotient}`)
+
+
+
+
+
+
+
