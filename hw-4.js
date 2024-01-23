1daf5c61cba0ed44093a599ad62a86186fe6cda8
Lada Antsyferova <mygi746@gmail.com>
2024-01-23 19:41:18 +0200

    hw-4

--- hw-4.js
+++ hw-4.js
@@ -5,14 +5,14 @@ let number2 = +prompt('Введіть друге число:');
 let sum = number1 + number2;
 let difference = number1 - number2;
 let product = number1 * number2;
-let quotient = number1 / number2;
+let division = number1 / number2;
 
 
-let newResult = alert(`Користувач ввів ${number1} і ${number2}:\n
+alert(`Користувач ввів ${number1} і ${number2}:\n
 ${number1} + ${number2} = ${sum} 
 ${number1} - ${number2} = ${difference} 
 ${number1} * ${number2} = ${product}
-${number1} / ${number2} = ${quotient}`)
+${number1} / ${number2} = ${division}`);
 
 
 
