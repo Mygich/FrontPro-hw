68db0c7c7a32406ec9a6be4b4602549f82eb87eb
Lada Antsyferova <mygi746@gmail.com>
2024-01-31 00:39:17 +0200

    hw-10

--- hw-10.js
+++ hw-10.js
@@ -22,3 +22,98 @@ console.log(count);
 console.log('end 1');
 
 // 2
+
+let min = arr[0];
+
+for(let i = 1; i < arr.length; i++){
+    if (arr[i] < min){
+        min = arr [i];
+    }
+}
+console.log(min);
+console.log(arr.indexOf(min));
+console.log('end 2');
+
+// 3
+let max = arr[0];
+
+for(let i = 1; i < arr.length; i++){
+    if (arr[i] > max){
+        max = arr [i];
+    }
+}
+console.log(max);
+console.log(arr.indexOf(max));
+console.log('end 3');
+
+// 4
+
+let negativeCount = 0;
+
+for (let i = 1; i < arr.length; i++){
+    if (arr[i] < 0 ){
+        negativeCount++;
+    }
+}
+console.log(negativeCount);
+console.log('end 4');
+
+// 5
+let unpairedPositiveCount = 0;
+
+for (let i = 1; i < arr.length; i++){
+    if (arr[i] > 0 && arr[i] %2 === 1){
+        unpairedPositiveCount++;
+    }
+}
+console.log(unpairedPositiveCount);
+console.log('end 5');
+
+// 6
+
+let doublePositiveCount = 0;
+
+for (let i = 1; i < arr.length; i++){
+    if (arr[i] > 0 && arr[i] %2 === 0){
+        doublePositiveCount++;
+    }
+}
+console.log(doublePositiveCount);
+console.log('end 6');
+
+// 7
+
+let doublePositiveSum = arr.reduce(function (accumulator, currentValue){
+    if(currentValue > 0 && currentValue %2 === 0){
+        return accumulator + currentValue;
+    } else {
+        return accumulator;
+    }
+},0);
+console.log(doublePositiveSum);
+console.log('end 7');
+
+// 8
+let unpairedPositiveSum = arr.reduce(function (accumulator, currentValue){
+    if (currentValue > 0 && currentValue %2 === 1){
+        return accumulator + currentValue;
+    } else {
+        return accumulator;
+    }
+}, 0);
+console.log(unpairedPositiveSum);
+console.log('end 8');
+
+// 9
+let positiveProduct = arr.reduce(function (accumulator,currentValue){
+    if (currentValue >= 1){
+        return accumulator * currentValue;
+    } else {
+        return accumulator;
+    }
+}, 1);
+console.log(positiveProduct);
+console.log('end 9');
+
+// 10
+
