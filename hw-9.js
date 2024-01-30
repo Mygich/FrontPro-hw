8dfe7e6f7e3f8638520d895055b237098314f506
Lada Antsyferova <mygi746@gmail.com>
2024-01-30 22:48:49 +0200

    hw-9.2

--- hw-9.js
+++ hw-9.js
@@ -1,5 +1,5 @@
 
-/*const array = [1, 2, 3, 4, 5, 6, 7];
+/*let array = [1, 2, 3, 4, 5, 6, 7];
 
 console.log(array.indexOf(5));
 
@@ -9,7 +9,7 @@ console.log(array);*/
 
 
 
-const array = [1, 2, 3, 4, 5, 6, 7];
+let array = [1, 2, 3, 4, 5, 6, 7];
 removeElement(array, 5);
 console.log(array);
 
