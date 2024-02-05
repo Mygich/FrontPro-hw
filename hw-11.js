8308fac81870201b76b52a7f427b6a2a2d5c5808
Lada Antsyferova <mygi746@gmail.com>
2024-02-05 02:09:47 +0200

    hw-11/2

--- hw-11.js
+++ hw-11.js
@@ -1,17 +1,15 @@
 
 
 function generateKey(length, characters){
-    let min = 0;
-    let max = characters.length;
     let result = '';
 
     for (let i = 0; i < length; i++){
-        let randomInd = Math.floor(Math.random() * (max - min + 1)) + min;
+        let randomInd = Math.floor(Math.random() * (characters.length + 1));
         result += characters[randomInd];
     }
     return result
 }
 const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-const key = generateKey(16, characters);
+const key = generateKey(5, characters);
 console.log(key);
 
