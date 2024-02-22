cb60ea32ff8b81522556f76793222d5ac79d83e8
Lada Antsyferova <mygi746@gmail.com>
2024-02-23 01:09:13 +0200

    hw-21\2

--- hw-21.js
+++ hw-21.js
@@ -1,6 +1,7 @@
 let images = ["./image/img.png", "./image/img_1.png", "./image/img_2.png", "./image/img_3.png", "./image/img_4.png", "./image/img_5.png", "./image/img_6.png", "./image/img_7.png", "./image/img_8.png", "./image/img_9.png"];
 let current = 0;
 
+//next
 function showNextImage() {
     current++;
     if (current >= images.length) {
@@ -9,6 +10,7 @@ function showNextImage() {
     document.getElementById("slider").children[0].src = images[current];
 }
 
+//prev
 function showPrevImage() {
     current--;
     if (current < 0) {
